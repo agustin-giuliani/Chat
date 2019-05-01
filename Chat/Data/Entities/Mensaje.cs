7de4a4fbff4161abd01a467ac62ed5cd0cea75c4
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-05-01 18:41:36 -0300

    Avance con Entity Framework.

---
+++ Chat/Data/Entities/Mensaje.cs
@@ -0,0 +1,38 @@
+﻿namespace Chat.Data.Entities
+{
+
+
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    public class Mensaje
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Enviado")]
+        public DateTime enviado { get; set; }
+
+        [Display(Name = "Mensaje")]
+        public string mensaje { get; set; }
+
+        [Display(Name = "Emisor")]
+        public Usuario usuarioEmisor { get; set; }
+
+        [Display(Name = "Receptor")]
+        public Usuario usuarioReceptor { get; set; }
+
+
+        [Display(Name = "Grupo")]
+        public int idGrupo { get; set; }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Chat/Data/Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chat.Data.Entities
+{
+    public class Usuario
+    {
+
+        public int id { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contraseña { get; set; }
+
+        public DateTime fechaAlta { get; set; }
+        public DateTime ultimoAcceso { get; set; }
+
+        public string preguntaSecreta { get; set; }
+
+        public string respuestaPregSecreta { get; set; }
+
+    }
+}
